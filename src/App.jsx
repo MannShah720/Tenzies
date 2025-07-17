82e87ef22f6b798213d143dd2985e6217bd5a6c3
MannShah720 <shahmann0108@gmail.com>
2025-07-17 17:34:01 +0100

    added die component (die is singular of dice) and function generate random numbers on dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Die from "./components/Die"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    const newDice = []
+
+    for (let i = 0; i < 10; i++) {
+      const randNum = Math.ceil(Math.random() * 6)
+      newDice.push(randNum)
+    }
+
+    return newDice
+  }
+
+  const diceElements = dice.map((num, index) => <Die key={index} val={num}/>)
 
   return (
-    <main></main>
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+    </main>
   )
 }
 
