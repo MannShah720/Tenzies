714c5861748955384caec0d0b1091432253de9cf
MannShah720 <shahmann0108@gmail.com>
2025-07-20 14:48:33 +0100

    added rollDice() , hold(id) so that the dice can freeze and be highlighted green

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
 import { useState } from 'react'
 import Die from "./components/Die"
 import './App.css'
+import { nanoid } from "nanoid"
 
 function App() {
 
-  const [dice, setDice] = useState(generateAllNewDice())
+  const [dice, setDice] = useState(generateAllNewDice())  // Array of objects
 
   function generateAllNewDice() {
     const newDice = []
 
     for (let i = 0; i < 10; i++) {
-      const randNum = Math.ceil(Math.random() * 6)  // Between 1-6
-      newDice.push(randNum)
+      const randNum = Math.ceil(Math.random() * 6)
+      newDice.push({
+        value: randNum,
+        isHeld: false,
+        id: nanoid()  // unique string ID generator
+      })
     }
 
     return newDice
   }
 
   function rollDice() {
-    setDice(generateAllNewDice)
+    setDice(oldDice => {
+      return oldDice.map(die => {
+        return die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
+      })
+    })
   }
 
-  const diceElements = dice.map((num, index) => <Die key={index} val={num}/>)
+  function hold(id) {
+    setDice(oldDice => {
+      return oldDice.map(die => {
+        return die.id === id ? {...die, isHeld:!die.isHeld} : die
+      })
+    })
+  }
+
+  const diceElements = dice.map((diceObject) => (
+  <Die 
+    key={diceObject.id}
+    value={diceObject.value}
+    isHeld={diceObject.isHeld}
+    hold={() => hold(diceObject.id)}
+  />))
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,3 +1,9 @@
 export default function Die(props) {
-    return <button>{props.val}</button>
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    
+    return (
+    <button style={styles} onClick={props.hold}>{props.value}</button>
+    )
 }
